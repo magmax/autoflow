8e900c04253ec69d181d0f0f4483b3c68d23befc
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-04-28 08:03:12 +0200

    Showing project graph

--- flow/service/urls.py
+++ flow/service/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<project>[^/]+)/(?P<status>[^/]+)/?', views.add_status),
+    url(r'^(?P<project>[^/]+)/?$', views.project),
+    url(r'^(?P<project>[^/]+)/(?P<status>[^/]+)/?$', views.status),
 ]
--- flow/service/views.py
+++ flow/service/views.py
@@ -4,10 +4,10 @@ from . import models
 from graphviz import Digraph
 
 
-def add_status(request, project, status):
+def status(request, project, status):
     method = dict(
-        GET=add_status_get,
-        POST=add_status_post,
+        GET=status_get,
+        POST=status_post,
     ).get(request.method)
     if method:
         st, _ = models.Status.objects.get_or_create(name=status)
@@ -19,7 +19,20 @@ def add_status(request, project, status):
     )
 
 
-def add_status_get(request, project, status):
+def project(request, project):
+    method = dict(
+        GET=project_get,
+    ).get(request.method)
+    if method:
+        prj, _ = models.Project.objects.get_or_create(name=project)
+        return method(request, prj)
+    raise http.Http404(
+        "Operation %s not implemented for url %s"
+        % (request.method, request.path)
+    )
+
+
+def status_get(request, project, status):
     getformat = request.GET.get('format')
     if getformat is None:
         return http.JsonResponse(
@@ -31,9 +44,17 @@ def add_status_get(request, project, status):
         render = RenderSvg(project, 'Status %s in project %s' % (status, project))
         response = http.HttpResponse(content_type=render.mime)
         render.add_status(status)
-        for st in status.transition_origin.all():
-            render.add_status(st.to_status)
-            render.add_transition(status, st.to_status)
+        transitions = set()
+        if request.GET.get('show_outputs') == 'true':
+            for st in status.transition_origin.all():
+                render.add_status(st.to_status)
+                transitions.add((status, st.to_status))
+        if request.GET.get('show_inputs') == 'true':
+            for st in status.transition_target.all():
+                render.add_status(st.to_status)
+                transitions.add((status, st.to_status))
+        for st_from, st_to in transitions:
+            render.add_transition(st_from, st_to)
         response.write(render.render())
         return response
 
@@ -64,7 +85,8 @@ class RenderSvg(object):
     def mime(self):
         return 'image/svg+xml'
 
-def add_status_post(request, project, status):
+
+def status_post(request, project, status):
     old_status = project.current_status
     project.current_status = status
     project.save()
@@ -79,5 +101,33 @@ def add_status_post(request, project, status):
     return http.JsonResponse(
         dict(
             result='ok',
+        )
+    )
+
+
+def project_get(request, project):
+    getformat = request.GET.get('format')
+    if getformat is None:
+        return http.JsonResponse(
+            dict(
+                result='Not implemented yet',
+            )
+        )
+    if getformat == 'svg':
+        render = RenderSvg(project, 'Status %s in project %s' % (status, project))
+        response = http.HttpResponse(content_type=render.mime)
+
+        for transition in project.transition_list.all():
+            st_in = transition.from_status
+            st_out = transition.to_status
+            render.add_status(st_in)
+            render.add_status(st_out)
+            render.add_transition(st_in, st_out)
+        response.write(render.render())
+        return response
+
+    return http.JsonResponse(
+        dict(
+            result='Unknown format',
         )
     )
